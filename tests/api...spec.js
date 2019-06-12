0d4779d27f3cb72318bf2eeb3bccfdfe7de61a96
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-06-12 18:28:10 -0500

    test del fetch y readdir cuando es directorio

--- tests/api...spec.js
+++ tests/api...spec.js
@@ -10,6 +10,7 @@ import {
 } from '../src/directory-controller.js'
 
 import { mdLinkExtractor } from '../src/links-controller.js';
+import { reccorerArr } from '../src/validate.js';
 
 describe('isAbsolute', () => {
     it('Debería ser una funcion', () => {
@@ -61,9 +62,56 @@ describe('readDir', () => {
         expect(typeof (readDir)).toEqual('function')
     });
     it('Debería recibir un path y retornar una array', () => {
-        expect(readDir('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo')).toEqual(['/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md',
-            '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-1/carpeta1-2/README.md',
-            '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'])
+        expect(readDir('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md')).toEqual([   {
+            href: 'https://code-maven.com/reading-a-file-with-nodejs',
+            text: 'hello estas',
+            file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md'
+          },
+          {
+            href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+            text: 'curriculum',
+            file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md'
+          }
+        ])
+    });
+    it('Debería recibir un path y retornar una array', () => {
+        expect(readDir('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo')).toEqual([
+            {
+              href: 'https://code-maven.com/reading-a-file-with-nodejs',
+              text: 'hello estas',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md'
+            },
+            {
+              href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+              text: 'curriculum',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md'
+            },
+            {
+              href: 'https://code-maven.com/reading-a-file-with-nodejs',
+              text: 'valeria',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-1/carpeta1-2/README.md'
+            },
+            {
+              href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+              text: 'apellido',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-1/carpeta1-2/README.md'
+            },
+            {
+              href: 'https://code-maven.com/reading-a-file-with-nodejs',
+              text: 'hola como estas',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+            },
+            {
+              href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+              text: 'elementos a considerar al escribir tu CV',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+            },
+            {
+              href: 'https://flippingbook.com/404',
+              text: 'error',
+              file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+            }
+          ])
     });
 })
 
@@ -72,18 +120,44 @@ describe('mdLinkExtractor', () => {
         expect(typeof (mdLinkExtractor)).toEqual('function')
     });
     it('Debería recibir un path y retornar una array de objetos', () => {
-        expect(mdLinkExtractor('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md')).toEqual([{
+        expect(mdLinkExtractor('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md')).toEqual([  {
             href: 'https://code-maven.com/reading-a-file-with-nodejs',
             text: 'hola como estas',
-            file:
-                '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
-        },
-        {
-            href:
-                'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+            file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+          },
+          {
+            href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
             text: 'elementos a considerar al escribir tu CV',
-            file:
-                '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
-        }])
+            file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+          },
+          {
+            href: 'https://flippingbook.com/404',
+            text: 'error',
+            file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/carpeta-2/README.md'
+          }
+        ])
     });
 })
+
+describe('reccorerArr', () => {
+    it('Deberia retornar un array resuelto de promesas', ()=>{
+        return reccorerArr(readDir('/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md'))
+        .then(data => {
+            expect(data).toEqual([{
+                href: 'https://code-maven.com/reading-a-file-with-nodejs',
+                text: 'hello estas',
+                file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md',
+                status: 200,
+                ok: 'OK'
+              },
+              {
+                href: 'https://drive.google.com/file/d/1TUHy3SxgalOWBqH-rtHKbejsKCXoLxWD/view?usp=sharing',
+                text: 'curriculum',
+                file: '/Users/valeriaberrocal/Desktop/LIM009-fe-md-links/ejemplo/README.md',
+                status: 200,
+                ok: 'OK'
+              },]);
+          });
+    })
+
+  });
